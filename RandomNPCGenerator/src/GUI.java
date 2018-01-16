f61f5fbf8f7fa500c8789c2033fe4be456c02769
Brady Klein <bklein827@gmail.com>
2018-01-16 14:20:56 -0600

    Testing GUI class

--- RandomNPCGenerator/src/GUI.java
+++ RandomNPCGenerator/src/GUI.java
@@ -1,21 +1,31 @@
+import java.awt.*;
+import java.awt.event.*;
 import java.swing.*;
 
-public class GUI{
-    //attributes
+public class GUI extends JPanel{
+    String[] type = { "Serif","SansSerif"};
+    int[] styles = { Font.PLAIN, Font.ITALIC, Font.BOLD, Font.ITALIC + Font.BOLD };
+    String[] stylenames = { "Plain", "Italic", "Bold", "Bold & Italic" };
 
-    //constructor
-    public GUI(){
-     JFrame frame = new JFrame("Random NPC Generator");
-     JPanel panel = new JPanel();
-     frame.add(panel);
-     Label test = new Label("test");
-     panel.add(test);
-     frame.setVisibile(true);
+    public void paint(Graphics g) {
+        for (int f = 0; f < type.length; f++) {
+            for (int s = 0; s < styles.length; s++) {
+                Font font = new Font(type[f], styles[s], 18);
+                g.setFont(font);
+                String name = type[f] + " " + stylenames[s];
+                g.drawString(name, 20, (f * 4 + s + 1) * 20);
+            }
+        }
     }
-    //methods
-
-    //main method
-    public static void main(String args[]){
-        new GUI();
+    public static void main(String[] a) {
+        JFrame f = new JFrame();
+        f.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        f.setContentPane(new Main());
+        f.setSize(400,400);
+        f.setVisible(true);
     }
 }
